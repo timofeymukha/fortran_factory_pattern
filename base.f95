b986b5ddf572138e7e15d621547cf9db116fe6dd
Timofey Mukha <timofey.mukha@protonmail.com>
2023-06-30 17:01:36 +0200

    Update

--- base.f95
+++ base.f95
@@ -3,12 +3,13 @@ module base
     
     !> The base class
     type, abstract :: base_t
+        integer :: a = 1
         contains
             procedure, nopass :: print_name
     end type
 contains
     subroutine print_name()
-        write(*,*) "I am type1_t"
+        write(*,*) "I am base_t"
     end subroutine
 
 
